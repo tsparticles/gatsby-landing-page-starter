bedb6e7617fd0a6c5491efb49f00cd86b5bf156d
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-07-05 14:27:48 +0200

    fix: fixed issues with 2.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,20 @@ import "../styles/index.css"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { loadFull } from "tsparticles";
 import Seo from "../components/Seo"
 
 library.add(fab)
 
 export default function Home() {
+  const particlesInit = async (engine) => {
+    await loadFull(engine);
+  };
+
   return (
     <div className="App">
       <Seo Sitetitle="Home" />
-      <Particles options={particlesOptions} />
+      <Particles init={particlesInit} options={particlesOptions} />
       <div className="flex-container">
         <div className="row">
           <h1 className="flex-item">Hello, I'm an John Doe</h1>
